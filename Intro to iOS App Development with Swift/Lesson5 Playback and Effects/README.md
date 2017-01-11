5276d33886a7ec803e8665db859c9c3015ec7553
SEUNG HOON, SHIN <newinh90@gmail.com>
2017-01-12 02:44:31 +0900

    : )

--- Intro to iOS App Development with Swift/Lesson5 Playback and Effects/README.md
+++ Intro to iOS App Development with Swift/Lesson5 Playback and Effects/README.md
@@ -27,9 +27,10 @@ Stack View 는 *Autolayout Constraints*를 간편하게 만들어줄거야.
 ![Wiring1](./Wiring1.png)
 
 1. 다음과 같이 StoryBoard에서 Play Sounds View Controller를 `ctrl`클릭 후 각 버튼에 연결지어준다.  
-![Wiring2](./Wirirng2.png)
+![Wiring2](./Wiring2.png)
 
 1. 이번에는 IBAction을 만들어보자. 먼저 코드를 작성하고
+
 ```swift
 // Mark : Actions
 
@@ -44,6 +45,86 @@ Stack View 는 *Autolayout Constraints*를 간편하게 만들어줄거야.
 
 1. 아까와는 반대로 버튼에서 Play Sounds View Controller로 이어주자
 ![Wiring3](./Wiring3.png)
+
+## Adding the Audio Extension
+
+- Paste the following code below the button outlets in `PlaySoundsViewController`:
+
+```swift
+var recordedAudioURL:URL!
+var audioFile:AVAudioFile!
+var audioEngine:AVAudioEngine!
+var audioPlayerNode: AVAudioPlayerNode!
+var stopTimer: Timer!
+
+enum ButtonType: Int {
+    case slow = 0, fast, chipmunk, vader, echo, reverb
+}
+```
+
+- 그리고 첨부된 PlaySoundsViewController+Audio.swift 파일을 우리 프로젝트에 넣자.
+아직 extention에 대해 잘 모르지만 일단 오디오를 출력하는 것에 집중하다.  
+- `viewDidLoad()` function에 `setupAudio()` 추가
+- `viewWillLoad()` function override하고 `configureUI(.notPlaying)` 추가
+
+## Playing Back Audio
+
+우리가 만든 6개의 UIButton들을 구별해야 하는데 가장쉬운 방법이 *tag property*를 사용하는 것이다. Attributes inspector에서 Tag 항목을 찾을 수있다. *tag property*에 단순 `Int` 값을 줄 수 있다. 건드리지 않았다면 `0`으로 초기화 되어있다.  
+0 ~ 5 를 각각 넣어주자.
+
+
+playSound function과 stopPlay function을 업데이트 해주자.
+```swift
+@IBAction func playSoundForButton(_ sender: UIButton){
+
+    switch(ButtonType(rawValue: sender.tag)!){
+
+    case .slow:
+        playSound()
+    case .fast:
+        playSound(rate: 1.5)
+    case .chipmunk:
+        playSound(pitch: 1000)
+    case .vader:
+        playSound(pitch: -1000)
+    case .echo:
+        playSound(echo: true)
+    case .reverb:
+        playSound(reverb: true)
+    }
+
+    configureUI(.playing)
+}
+
+@IBAction func stopButtonPressed(_ sender: AnyObject){
+    stopAudio()
+}
+
+```
+
+
+
+## playSoundViewController Extension
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int{
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
     
     // MARK: Outlets
     
@@ -25,36 +34,39 @@ class PlaySoundsViewController: UIViewController {
     // Mark : Actions
     
     @IBAction func playSoundForButton(_ sender: UIButton){
-        print("Play Sound Button Pressed")
+        
+        switch(ButtonType(rawValue: sender.tag)!){
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject){
-        print("Stop Audio Button Pressed")
+        stopAudio()
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(recordedAudioURL)
+        setupAudio()
+//        print(recordedAudioURL)
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
